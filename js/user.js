9fb41305200a8ff28eb43c41eb63ab2cae7453fd
Monzoor Morshed <mmtamal@gmail.com>
2015-06-12 19:45:18 +0600

    added changes

--- js/user.js
+++ js/user.js
@@ -1,6 +1,20 @@
 $( document ).ready(function() {
+	$("section").height($(window).height());
 
+	 var stickyNav = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > 300) {
+            $('header').addClass('selected');
+        } else {
+            $('header').removeClass('selected');
+        }
+    };
+
+    
+
+    $(window).scroll(function () {
+    	console.log("test")
+        stickyNav();
+    });
 })
-
-
 
